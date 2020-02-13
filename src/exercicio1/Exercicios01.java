51a5c61dd592a98156b761967f180765509a9ba6
Treinamento <Treinamento@RD723935.raiadrogasil.com>
2020-02-13 13:06:05 -0300

    exercicios1

---
+++ src/exercicio1/Exercicios01.java
@@ -0,0 +1,10 @@
+public class Exercicios01 {
+    public static void main(String[] args){
+        Futebol futebol= new Futebol("titular","Corinthians","Sao Paulo");
+        futebol.getEstado();
+        futebol.getJogador();
+        futebol.getTime();
+
+    }
+
+}
---
+++ src/exercicio1/Futebol.java
@@ -0,0 +1,38 @@
+public class Futebol {
+    private String jogador;
+    private String time;
+    private String estado;
+
+    public Futebol(String jogador, String time, String estado) {
+        this.jogador = jogador;
+        this.time = time;
+        this.estado = estado;
+    }
+
+    public String getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(String jogador) {
+        this.jogador = jogador;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public void futebol(){
+        System.out.println(" vai corinthians "+ null);
+    }
+}
